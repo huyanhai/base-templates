c628b0211d19f68a7c61f726dcfcde3fa3a3539f
Huyanhai <810839700@qq.com>
2025-06-20 16:29:58 +0800

    style: 样式修改

--- src/views/NotFountPage.vue
+++ src/views/NotFountPage.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="flex items-center justify-center h-screen w-screen text-8xl"
-  >
+  <div class="flex items-center justify-center h-screen w-screen text-8xl">
     <p class="-translate-y-20 text-cyan-700">404</p>
   </div>
 </template>
